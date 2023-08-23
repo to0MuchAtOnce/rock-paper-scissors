6ac3e14d67a886a514be05f7a84b29600cad08ef
Tom Lewis <tom.lewis488@gmail.com>
2023-08-23 21:59:29 +0200

    Updated logic

--- app.js
+++ app.js
@@ -3,15 +3,50 @@ const choice = ['rock', 'paper', 'scissors'];
 // Randomly returns an item from the 'choice' array.
 function getComputerChoice() {
   const random = Math.floor(Math.random() * choice.length);
-  console.log(random, choice[random]);
+  const computerSelection = choice[random];
+  console.log('Computer:', computerSelection);
+  return computerSelection;
 }
 
+// Play 1 round of the game
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return 'Its a draw!';
+  const lowerCasePlayerSelection = playerSelection.toLowerCase();
+  if (lowerCasePlayerSelection === computerSelection) {
+    return "It's a draw";
+  } else if (
+    (lowerCasePlayerSelection === 'rock' && computerSelection === 'scissors') ||
+    (lowerCasePlayerSelection === 'paper' && computerSelection === 'rock') ||
+    (lowerCasePlayerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    return (
+      'You win, ' + lowerCasePlayerSelection + 'beats ' + computerSelection
+    );
+  } else {
+    return (
+      'You loose, ' + computerSelection + 'beats ' + lowerCasePlayerSelection
+    );
   }
 }
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = 'rock';
+    const computerSelection = getComputerChoice();
+    const roundResult = playRound(playerSelection, computerSelection);
+    console.log(roundResult);
+
+    if (roundResult.includes('win')) {
+      playerScore++;
+      console.log('Player score:', playerScore);
+    } else if (roundResult.includes('loose')) {
+      computerScore++;
+      console.log('Computer score:', playerScore);
+    }
+  }
+
+  console.log(`FInal score: Player ${playerScore} - Computer ${computerScore}`);
+}
+
+game();
